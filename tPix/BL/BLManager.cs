f6c7655939d14877e761b49d312fd4e461bc51cb
smithb024 <bertram.smith@gmail.com>
2025-10-19 22:10:09 +0100

    #8: Start successfully

--- tPix/BL/BLManager.cs
+++ tPix/BL/BLManager.cs
@@ -1,318 +1,317 @@
 ﻿namespace tPix.BL
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Collections.ObjectModel;
-  using Common.Enum;
-  using tPix.BL.Interfaces;
-  using tPix.BL.Interfaces.ClsNmbConfig;
-  using tPix.BL.Model.ClsNmbConfig;
-  using tPix.Common;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Common.Enum;
+    using tPix.BL.Interfaces;
+    using tPix.BL.Interfaces.ClsNmbConfig;
+    using tPix.BL.Model.ClsNmbConfig;
+    using tPix.Common;
 
-  public class BLManager
-  {
-    private Random randomGenerator;
-    private Model.FaultManager faultManager;
-    private Model.LocationManager locationManager;
-    private IClsNmbManager clsNmbManager;
-    private List<IImageDetails> allImages;
-    private string basePath;
+    public class BLManager
+    {
+        private Random randomGenerator;
+        private Model.FaultManager faultManager;
+        private Model.LocationManager locationManager;
+        private IClsNmbManager clsNmbManager;
+        private List<IImageDetails> allImages;
+        private string basePath;
 
         /// <summary>
         /// Initialises a new instance of the <see cref="BLManager"/> class.
         /// </summary>
         public BLManager()
         {
-            //basePath = "C:\\_myDocs\\bert\\03_projects\\my_programing\\cSharpWPF\\tPix\\tPix\\bin\\Debug//testDb";
-            basePath = "C:\\_myDocs\\bert\\01_common_stuff\\ba_0811\\pics";
-            randomGenerator = new Random();
+            this.basePath = "C:\\tp";
+            this.randomGenerator = new Random();
 
-            Factories.ClsNmbFactory clsNmbReader = new BL.Factories.ClsNmbFactory(basePath);
-            Factories.ImageReader reader = new BL.Factories.ImageReader(basePath);
+            Factories.ClsNmbFactory clsNmbReader = new BL.Factories.ClsNmbFactory(this.basePath);
+            Factories.ImageReader reader = new BL.Factories.ImageReader(this.basePath);
 
             this.faultManager = new BL.Model.FaultManager();
 
             this.locationManager =
-              new BL.Model.LocationManager(
-                basePath,
-                faultManager);
+                new BL.Model.LocationManager(
+                    this.basePath,
+                    this.faultManager);
             this.clsNmbManager =
-              new ClsNmbManager(
-              clsNmbReader.ReadClsDetails(
-                faultManager));
+                new ClsNmbManager(
+                    clsNmbReader.ReadClsDetails(
+                        this.faultManager));
 
             this.allImages =
-              reader.ReadImages(
-              locationManager,
-              clsNmbManager,
-              faultManager);
+                reader.ReadImages(
+                    this.locationManager,
+                    this.clsNmbManager,
+                    this.faultManager);
 
-            locationManager.Save();
+            this.locationManager.Save();
         }
 
-    public string BasePath => this.basePath;
+        public string BasePath => this.basePath;
 
-    public ObservableCollection<string> GetClasses()
-    {
-      List<string> classes = new List<string>();
-
-      foreach (IImageDetails image in this.allImages)
-      {
-        foreach (ICls cls in image.Clss)
+        public ObservableCollection<string> GetClasses()
         {
-          if (!classes.Contains(cls.Name))
-          {
-            classes.Add(cls.Name);
-          }
-        }
-      }
+            List<string> classes = new List<string>();
 
-      classes.Sort();
-      return this.Convert(classes);
-    }
-
-    public ObservableCollection<string> GetNmbs(string clsName)
-    {
-      List<string> nmbs = new List<string>();
-
-      foreach (IImageDetails image in this.allImages)
-      {
-        foreach (ICls searchCls in image.Clss)
-        {
-          if (string.Equals(searchCls.Name, clsName))
-          {
-            foreach (string nmb in searchCls.Nmbs)
+            foreach (IImageDetails image in this.allImages)
             {
-              if (!nmbs.Contains(nmb))
-              {
-                nmbs.Add(nmb);
-              }
+                foreach (ICls cls in image.Clss)
+                {
+                    if (!classes.Contains(cls.Name))
+                    {
+                        classes.Add(cls.Name);
+                    }
+                }
             }
-          }
+
+            classes.Sort();
+            return this.Convert(classes);
         }
-      }
 
-      nmbs.Sort();
-      return this.Convert(nmbs);
-    }
-
-    public ObservableCollection<string> GetLocations()
-    {
-      return this.Convert(this.locationManager.LocationsByName);
-    }
-
-    public ObservableCollection<ILocation> GetLocationsByLetter(string character)
-    {
-      return this.Convert(this.locationManager.GetLocationsByLetter(character));
-    }
-
-    public void SaveLocation(ILocation location)
-    {
-      this.locationManager.UpdateLocation(location);
-    }
-
-    public ObservableCollection<string> GetLines()
-    {
-      return this.Convert(this.locationManager.Lines);
-    }
-
-    public ObservableCollection<string> GetCounties()
-    {
-      return this.Convert(this.locationManager.Counties);
-    }
-
-    public ObservableCollection<string> GetRegions()
-    {
-      return this.Convert(this.locationManager.Regions);
-    }
-
-    public ObservableCollection<string> GetBig4Regions()
-    {
-      return this.Convert(this.locationManager.Big4Regions);
-    }
-
-    public ImageDescription GetImage()
-    {
-      int r = randomGenerator.Next(this.allImages.Count);
-      return new ImageDescription(
-        this.allImages[r].Path,
-        this.allImages[r].Description);
-    }
-
-    public ObservableCollection<ImageDescription> GetImages(
-      string clsName)
-    {
-      return this.Convert(
-        this.GetImageDetails(
-          clsName));
-    }
-
-    public ObservableCollection<ImageDescription> GetImages(
-      string clsName,
-      string nmb)
-    {
-      return this.Convert(
-        this.GetImageDetails(
-          clsName,
-          nmb));
-    }
-
-    public ObservableCollection<ImageDescription> GetImages(
-      LocationType locationType,
-      string location)
-    {
-      List<IImageDetails> imageList =
-        this.GetImageDetails(
-          this.allImages,
-          locationType,
-          location);
-
-      return this.Convert(imageList);
-    }
-
-    public ObservableCollection<ImageDescription> GetImages(
-      string clsName,
-      LocationType locationType,
-      string location)
-    {
-      List<IImageDetails> imageList =
-        this.GetImageDetails(
-          clsName);
-
-      imageList =
-        this.GetImageDetails(
-          imageList,
-          locationType,
-          location);
-
-      return this.Convert(imageList);
-    }
-
-    public ObservableCollection<ImageDescription> GetImages(
-      string clsName,
-      string nmb,
-      LocationType locationType,
-      string location)
-    {
-      List<IImageDetails> imageList =
-        this.GetImageDetails(
-          clsName,
-          nmb);
-
-      imageList =
-        this.GetImageDetails(
-          imageList,
-          locationType,
-          location);
-
-      return this.Convert(imageList);
-    }
-
-    private ObservableCollection<string> Convert(List<string> origCollection)
-    {
-      ObservableCollection<string> outputCollection = new ObservableCollection<string>();
-
-      if (origCollection != null)
-      {
-        foreach (string item in origCollection)
+        public ObservableCollection<string> GetNmbs(string clsName)
         {
-          outputCollection.Add(item);
+            List<string> nmbs = new List<string>();
+
+            foreach (IImageDetails image in this.allImages)
+            {
+                foreach (ICls searchCls in image.Clss)
+                {
+                    if (string.Equals(searchCls.Name, clsName))
+                    {
+                        foreach (string nmb in searchCls.Nmbs)
+                        {
+                            if (!nmbs.Contains(nmb))
+                            {
+                                nmbs.Add(nmb);
+                            }
+                        }
+                    }
+                }
+            }
+
+            nmbs.Sort();
+            return this.Convert(nmbs);
         }
-      }
 
-      return outputCollection;
-    }
-
-    private ObservableCollection<ImageDescription> Convert(List<IImageDetails> origCollection)
-    {
-      ObservableCollection<ImageDescription> outputCollection = new ObservableCollection<ImageDescription>();
-
-      if (origCollection != null)
-      {
-        foreach (IImageDetails item in origCollection)
+        public ObservableCollection<string> GetLocations()
         {
-          outputCollection.Add(
-            new ImageDescription(
-              item.Path,
-              item.Description));
+            return this.Convert(this.locationManager.LocationsByName);
         }
-      }
 
-      return outputCollection;
-    }
-
-    private ObservableCollection<ILocation> Convert(List<ILocation> origCollection)
-    {
-      ObservableCollection<ILocation> outputCollection = new ObservableCollection<ILocation>();
-
-      if (origCollection != null)
-      {
-        foreach (ILocation item in origCollection)
+        public ObservableCollection<ILocation> GetLocationsByLetter(string character)
         {
-          outputCollection.Add(item);
+            return this.Convert(this.locationManager.GetLocationsByLetter(character));
         }
-      }
 
-      return outputCollection;
-    }
+        public void SaveLocation(ILocation location)
+        {
+            this.locationManager.UpdateLocation(location);
+        }
 
-    private int GetLineIndex(string line)
-    {
-      return this.locationManager.Lines.FindIndex(l => l == line);
-    }
+        public ObservableCollection<string> GetLines()
+        {
+            return this.Convert(this.locationManager.Lines);
+        }
 
-    private int GetCountyIndex(string county)
-    {
-      return this.locationManager.Counties.FindIndex(l => l == county);
-    }
+        public ObservableCollection<string> GetCounties()
+        {
+            return this.Convert(this.locationManager.Counties);
+        }
 
-    private int GetRegionIndex(string region)
-    {
-      return this.locationManager.Regions.FindIndex(l => l == region);
-    }
+        public ObservableCollection<string> GetRegions()
+        {
+            return this.Convert(this.locationManager.Regions);
+        }
 
-    private int GetBig4Index(string big4)
-    {
-      return this.locationManager.Big4Regions.FindIndex(l => l == big4);
-    }
+        public ObservableCollection<string> GetBig4Regions()
+        {
+            return this.Convert(this.locationManager.Big4Regions);
+        }
 
-    private List<IImageDetails> GetImageDetails(
-      string clsName)
-    {
-      return this.allImages.FindAll(i => i.ContainsCls(clsName));
-    }
+        public ImageDescription GetImage()
+        {
+            int r = randomGenerator.Next(this.allImages.Count);
+            return new ImageDescription(
+              this.allImages[r].Path,
+              this.allImages[r].Description);
+        }
 
-    private List<IImageDetails> GetImageDetails(
-      string clsName,
-      string nmb)
-    {
-      return this.allImages.FindAll(i => i.ContainsCls(clsName) && i.ContainsNmb(nmb));
-    }
+        public ObservableCollection<ImageDescription> GetImages(
+          string clsName)
+        {
+            return this.Convert(
+              this.GetImageDetails(
+                clsName));
+        }
 
-    private List<IImageDetails> GetImageDetails(
-      List<IImageDetails> images,
-      LocationType locationType,
-      string location)
-    {
-      switch (locationType)
-      {
-        case LocationType.Location:
-          return images.FindAll(i => i.Stn.Name == location);
-        case LocationType.Line:
-          return images.FindAll(
-              i => i.Stn.Line == this.GetLineIndex(location));
-        case LocationType.County:
-          return images.FindAll(
-              i => i.Stn.County == this.GetCountyIndex(location));
-        case LocationType.Region:
-          return images.FindAll(
-              i => i.Stn.Region == this.GetRegionIndex(location));
-        case LocationType.Big4Location:
-          return images.FindAll(
-              i => i.Stn.Big4 == this.GetBig4Index(location));
-        default:
-          return new List<IImageDetails>();
-      }
+        public ObservableCollection<ImageDescription> GetImages(
+          string clsName,
+          string nmb)
+        {
+            return this.Convert(
+              this.GetImageDetails(
+                clsName,
+                nmb));
+        }
+
+        public ObservableCollection<ImageDescription> GetImages(
+          LocationType locationType,
+          string location)
+        {
+            List<IImageDetails> imageList =
+              this.GetImageDetails(
+                this.allImages,
+                locationType,
+                location);
+
+            return this.Convert(imageList);
+        }
+
+        public ObservableCollection<ImageDescription> GetImages(
+          string clsName,
+          LocationType locationType,
+          string location)
+        {
+            List<IImageDetails> imageList =
+              this.GetImageDetails(
+                clsName);
+
+            imageList =
+              this.GetImageDetails(
+                imageList,
+                locationType,
+                location);
+
+            return this.Convert(imageList);
+        }
+
+        public ObservableCollection<ImageDescription> GetImages(
+          string clsName,
+          string nmb,
+          LocationType locationType,
+          string location)
+        {
+            List<IImageDetails> imageList =
+              this.GetImageDetails(
+                clsName,
+                nmb);
+
+            imageList =
+              this.GetImageDetails(
+                imageList,
+                locationType,
+                location);
+
+            return this.Convert(imageList);
+        }
+
+        private ObservableCollection<string> Convert(List<string> origCollection)
+        {
+            ObservableCollection<string> outputCollection = new ObservableCollection<string>();
+
+            if (origCollection != null)
+            {
+                foreach (string item in origCollection)
+                {
+                    outputCollection.Add(item);
+                }
+            }
+
+            return outputCollection;
+        }
+
+        private ObservableCollection<ImageDescription> Convert(List<IImageDetails> origCollection)
+        {
+            ObservableCollection<ImageDescription> outputCollection = new ObservableCollection<ImageDescription>();
+
+            if (origCollection != null)
+            {
+                foreach (IImageDetails item in origCollection)
+                {
+                    outputCollection.Add(
+                      new ImageDescription(
+                        item.Path,
+                        item.Description));
+                }
+            }
+
+            return outputCollection;
+        }
+
+        private ObservableCollection<ILocation> Convert(List<ILocation> origCollection)
+        {
+            ObservableCollection<ILocation> outputCollection = new ObservableCollection<ILocation>();
+
+            if (origCollection != null)
+            {
+                foreach (ILocation item in origCollection)
+                {
+                    outputCollection.Add(item);
+                }
+            }
+
+            return outputCollection;
+        }
+
+        private int GetLineIndex(string line)
+        {
+            return this.locationManager.Lines.FindIndex(l => l == line);
+        }
+
+        private int GetCountyIndex(string county)
+        {
+            return this.locationManager.Counties.FindIndex(l => l == county);
+        }
+
+        private int GetRegionIndex(string region)
+        {
+            return this.locationManager.Regions.FindIndex(l => l == region);
+        }
+
+        private int GetBig4Index(string big4)
+        {
+            return this.locationManager.Big4Regions.FindIndex(l => l == big4);
+        }
+
+        private List<IImageDetails> GetImageDetails(
+          string clsName)
+        {
+            return this.allImages.FindAll(i => i.ContainsCls(clsName));
+        }
+
+        private List<IImageDetails> GetImageDetails(
+          string clsName,
+          string nmb)
+        {
+            return this.allImages.FindAll(i => i.ContainsCls(clsName) && i.ContainsNmb(nmb));
+        }
+
+        private List<IImageDetails> GetImageDetails(
+          List<IImageDetails> images,
+          LocationType locationType,
+          string location)
+        {
+            switch (locationType)
+            {
+                case LocationType.Location:
+                    return images.FindAll(i => i.Stn.Name == location);
+                case LocationType.Line:
+                    return images.FindAll(
+                        i => i.Stn.Line == this.GetLineIndex(location));
+                case LocationType.County:
+                    return images.FindAll(
+                        i => i.Stn.County == this.GetCountyIndex(location));
+                case LocationType.Region:
+                    return images.FindAll(
+                        i => i.Stn.Region == this.GetRegionIndex(location));
+                case LocationType.Big4Location:
+                    return images.FindAll(
+                        i => i.Stn.Big4 == this.GetBig4Index(location));
+                default:
+                    return new List<IImageDetails>();
+            }
+        }
     }
-  }
 }
--- tPix/BL/Factories/ImageDetailFactory.cs
+++ tPix/BL/Factories/ImageDetailFactory.cs
@@ -6,151 +6,166 @@ using System.Threading.Tasks;
 
 namespace tPix.BL.Factories
 {
-  using Interfaces;
-  using Interfaces.ClsNmbConfig;
-  using Model;
+    using Interfaces;
+    using Interfaces.ClsNmbConfig;
+    using Model;
 
-  public static class ImageDetailFactory
-  {
-    private static char majorTick = '-';
-    private static char minorTick = '_';
-    private static char alternativeTick = '~';
-    private static char clsTick = '^';
-    private static char extensionSeparator = '.';
-
-    private class ClsClass
+    public static class ImageDetailFactory
     {
-      public ClsClass(
-        List<ICls> clss,
-        List<string> presentNmbs)
-      {
-        this.Clss = clss;
-        this.PresentNmbs = presentNmbs;
-      }
+        private static char majorTick = '-';
+        private static char minorTick = '_';
+        private static char alternativeTick = '~';
+        private static char clsTick = '^';
+        private static char extensionSeparator = '.';
 
-      public List<ICls> Clss { get; }
-      public List<string> PresentNmbs { get; }
-    }
-
-    public static IImageDetails AnalyseImageDetails(
-      string path,
-      ILocationManager locationManager,
-      IClsNmbManager clsNmbManager,
-      IFaultManager faultManager)
-    {
-      IImageDetails image;
-
-      string imageName = System.IO.Path.GetFileName(path);
-
-      string[] filenameArray = imageName.Split(extensionSeparator);
-
-      string[] inputArray    = filenameArray[0].Split(majorTick);
-
-      if (inputArray.Length < 2 || inputArray.Length > 4)
-      {
-        faultManager.AddFault(
-          "Can't split into nmb and stn",
-          imageName);
-      }
-
-      string[] nmbsArray   = inputArray[0].Split(minorTick);
-
-      ILocation stn =
-        locationManager.GetStn(
-          inputArray[1]);
-
-      string year =
-        inputArray.Length > 2 ?
-        inputArray[2] :
-        string.Empty;
-
-      string multipleNote =
-        inputArray.Length > 3 ?
-        inputArray[3] :
-        string.Empty;
-
-      image =
-        new ImageDetails(
-          path,
-          year,
-          stn,
-          multipleNote);
-
-      ClsClass clss =
-        ImageDetailFactory.GetCls(
-          nmbsArray.ToList(),
-          path,
-          faultManager,
-          clsNmbManager);
-      image.SetClss(
-        clss.Clss,
-        clss.PresentNmbs);
-
-      return image;
-    }
-
-    private static ClsClass GetCls(
-      List<string> nmbs,
-      string imageName,
-      IFaultManager faultManager,
-      IClsNmbManager clsNmbManager)
-    {
-      List<ICls> clss = new List<ICls>();
-      List<string> present = new List<string>();
-
-      foreach (string nmb in nmbs)
-      {
-        string[] individualNmbArray   = nmb.Split(clsTick);
-        if (individualNmbArray.Length != 2)
+        private class ClsClass
         {
-          faultManager.AddFault(
-            "Numbers Incorrect",
-            imageName);
-          continue;
-        }
-
-        present.Add(individualNmbArray[1]);
-
-        // Go to nmb manager and get all associated nmbs
-        //List<string> foundNmbs = new List<string>() { individualNmbArray[1] };
-        List<string> foundNmbs =
-          clsNmbManager.FindAllNmbs(
-            individualNmbArray[0],
-            individualNmbArray[1]);
-        bool foundCls = false;
-
-        foreach (ICls cls in clss)
-        {
-          if (cls.Name == individualNmbArray[0])
-          {
-            foreach (string foundNmb in foundNmbs)
+            public ClsClass(
+              List<ICls> clss,
+              List<string> presentNmbs)
             {
-              cls.AddNmb(foundNmb);
+                this.Clss = clss;
+                this.PresentNmbs = presentNmbs;
             }
 
-            foundCls = true;
-            break;
-          }
+            public List<ICls> Clss { get; }
+            public List<string> PresentNmbs { get; }
         }
 
-        if (!foundCls)
+        public static IImageDetails AnalyseImageDetails(
+          string path,
+          ILocationManager locationManager,
+          IClsNmbManager clsNmbManager,
+          IFaultManager faultManager)
         {
-          ICls newCls =
-            new Cls(
-              individualNmbArray[0]);
-          foreach (string foundNmb in foundNmbs)
-          {
-            newCls.AddNmb(foundNmb);
-          }
+            string imageName = string.Empty;
 
-          clss.Add(newCls);
+            try
+            {
+                IImageDetails image;
+
+                imageName = System.IO.Path.GetFileName(path);
+
+                string[] filenameArray = imageName.Split(extensionSeparator);
+
+                string[] inputArray = filenameArray[0].Split(majorTick);
+
+                if (inputArray.Length < 2 || inputArray.Length > 4)
+                {
+                    faultManager.AddFault(
+                      "Can't split into nmb and stn",
+                      imageName);
+                    return null;
+                }
+
+                string[] nmbsArray = inputArray[0].Split(minorTick);
+
+                ILocation stn =
+                  locationManager.GetStn(
+                    inputArray[1]);
+
+                string year =
+                  inputArray.Length > 2 ?
+                  inputArray[2] :
+                  string.Empty;
+
+                string multipleNote =
+                  inputArray.Length > 3 ?
+                  inputArray[3] :
+                  string.Empty;
+
+                image =
+                  new ImageDetails(
+                    path,
+                    year,
+                    stn,
+                    multipleNote);
+
+                ClsClass clss =
+                  ImageDetailFactory.GetCls(
+                    nmbsArray.ToList(),
+                    path,
+                    faultManager,
+                    clsNmbManager);
+                image.SetClss(
+                  clss.Clss,
+                  clss.PresentNmbs);
+
+                return image;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                faultManager.AddFault(
+                    "Can't split into nmb and stn",
+                    imageName);
+
+                return null;
+            }
         }
-      }
 
-      return 
-        new ClsClass(
-          clss,
-          present);
+        private static ClsClass GetCls(
+          List<string> nmbs,
+          string imageName,
+          IFaultManager faultManager,
+          IClsNmbManager clsNmbManager)
+        {
+            List<ICls> clss = new List<ICls>();
+            List<string> present = new List<string>();
+
+            foreach (string nmb in nmbs)
+            {
+                string[] individualNmbArray = nmb.Split(clsTick);
+                if (individualNmbArray.Length != 2)
+                {
+                    faultManager.AddFault(
+                      "Numbers Incorrect",
+                      imageName);
+                    continue;
+                }
+
+                present.Add(individualNmbArray[1]);
+
+                // Go to nmb manager and get all associated nmbs
+                //List<string> foundNmbs = new List<string>() { individualNmbArray[1] };
+                List<string> foundNmbs =
+                  clsNmbManager.FindAllNmbs(
+                    individualNmbArray[0],
+                    individualNmbArray[1]);
+                bool foundCls = false;
+
+                foreach (ICls cls in clss)
+                {
+                    if (cls.Name == individualNmbArray[0])
+                    {
+                        foreach (string foundNmb in foundNmbs)
+                        {
+                            cls.AddNmb(foundNmb);
+                        }
+
+                        foundCls = true;
+                        break;
+                    }
+                }
+
+                if (!foundCls)
+                {
+                    ICls newCls =
+                      new Cls(
+                        individualNmbArray[0]);
+                    foreach (string foundNmb in foundNmbs)
+                    {
+                        newCls.AddNmb(foundNmb);
+                    }
+
+                    clss.Add(newCls);
+                }
+            }
+
+            return
+              new ClsClass(
+                clss,
+                present);
+        }
     }
-  }
 }
